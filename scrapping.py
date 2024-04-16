93beb0c7f3aa8d648b7c3cdedc42796657043a6d
abou94190 <abouly25@gmail.com>
2024-04-16 12:41:47 +0200

    ajoutcommentaire

--- scrapping.py
+++ scrapping.py
@@ -39,13 +39,13 @@ def scrape_amazon_books(url):
     
     return livres
 
-def main():
+def affichage():
     url = 'https://www.amazon.fr/kindle-dbs/browse?metadata=cardAppType%3ADESKTOP%24deviceTypeID%3AA2Y8LFC259B97P%24clientRequestId%3AY050AM5KD770854NFYY6%24deviceAppType%3ADESKTOP%24ipAddress%3A10.90.176.80%24browseNodes%3A695398031%24userAgent%3AMozilla%2F5.0+%28Windows+NT+10.0%3B+Win64%3B+x64%3B+rv%3A124.0%29+Gecko%2F20100101+Firefox%2F124.0%24cardSurfaceType%3Adesktop%24cardMobileOS%3AUnknown%24locale%3Afr_FR%24deviceSurfaceType%3Adesktop&storeType=ebooks&widgetId=unified-ebooks-storefront-default_TopSellersStrategy&sourceAsin=&content-id=amzn1.sym.360aebec-b11d-4773-8755-47be2f158110&refTagFromService=ts&title=Meilleures%20Ventes&pf_rd_p=360aebec-b11d-4773-8755-47be2f158110&sourceType=recs&pf_rd_r=Y050AM5KD770854NFYY6&pd_rd_wg=8A2IT&ref_=dbs_r_recs_ts_r&SkipDeviceExclusion=true&page=1&pd_rd_w=wCpwS&nodeId=695398031&pd_rd_r=155e4e23-da00-4753-89c8-5ce26c805f69'
     
-    livres = scrape_amazon_books(url)
+    livres = scrape_amazon_books(url)  # utilisation de la fonction scrap
     
-    for nom, prix in livres:
+    for nom, prix in livres:  # affichage des listes fusioné
         print(f"{nom}: {prix}")
 
 if __name__ == "__main__":
-    main()
+    affichage()
